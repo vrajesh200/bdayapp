7bcdd82200099a3d3c2add1b23c92c77354bc8e3
psrinivas <psrinivas@gmail.com>
2010-08-22 10:58:08 +0000

    Display list of contacts retrieved from phone

--- src/com/bdayapp/BdayNotifier.java
+++ src/com/bdayapp/BdayNotifier.java
@@ -1,14 +1,43 @@
 package com.bdayapp;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class BdayNotifier extends Activity
 {
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        ListView mContactList;
+        mContactList = (ListView)findViewById(R.id.contact_list);
+        Cursor contactCursor = getContactList();
+        String[] fields = new String[] {
+                ContactsContract.Data.DISPLAY_NAME               
+        };
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.list_item, contactCursor,
+                fields, new int[] {R.id.contact_name_});
+        mContactList.setAdapter(adapter);
+    }
+    
+    private Cursor getContactList()
+    {
+    	Uri uri = ContactsContract.Contacts.CONTENT_URI;
+    	String[] BdayList = new String[] {
+    			ContactsContract.Contacts._ID,
+    			ContactsContract.Contacts.DISPLAY_NAME
+        };
+        String selection = ContactsContract.Contacts.IN_VISIBLE_GROUP + " = '" +
+        "1" + "'";
+    	String sortOrder = ContactsContract.Contacts.DISPLAY_NAME;
+    	String[] selectionArgs = null;
+    	return managedQuery(uri, BdayList, selection, selectionArgs, sortOrder);
     }
 }
