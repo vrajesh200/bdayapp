94a8c9be16df226a33b25fe7b0834c8b9f509c0b
rajesh.born2win <rajesh.v200@gmail.com>
2010-08-23 20:21:38 +0000

    clean @override annotations

--- src/com/bdayapp/bdayList.java
+++ src/com/bdayapp/bdayList.java
@@ -18,36 +18,33 @@ public class bdayList extends BaseAdapter {
 		elements = list;
 		c = cxt;
 	}
-	@Override
+
 	public int getCount() {
 		return elements.size();
 	}
 
-	@Override
 	public Object getItem(int position) {
 		// TODO Auto-generated method stub
 		return elements.get(position);
 	}
 
-	@Override
 	public long getItemId(int position) {
 		// TODO Auto-generated method stub
 		return position;
 	}
 
-	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		TextView tview_cname;
 		TextView tview_dob;
 		View v = convertView;
-		
+
         if (v == null) {
         	v = View.inflate(c, R.layout.list_item, null);
         }
-        
+
         tview_cname = (TextView)v.findViewById(R.id.contact_name);
         tview_cname.setText((elements.get(position)).contactName.toString());
-        
+
         tview_dob = (TextView)v.findViewById(R.id.date_of_birth);
         tview_dob.setText((elements.get(position)).dateOfBirth.toString());
         return v;
