027b9d44052e14d2160fe148b7853654d0a9e0fc
rajesh.born2win <rajesh.v200@gmail.com>
2010-08-25 12:20:30 +0000

    serialize ContactInfo into bytes

--- src/com/bdayapp/contacts/ContactInfo.java
+++ src/com/bdayapp/contacts/ContactInfo.java
@@ -1,12 +1,20 @@
 package com.bdayapp.contacts;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.Date;
 
 import android.net.Uri;
 
 import com.bdayapp.Utils;
 
-public class ContactInfo implements Comparable<ContactInfo> {
+public class ContactInfo implements Comparable<ContactInfo>, Serializable {
+	private static final long serialVersionUID = -2903391245741553194L;
+
 	private String contactId;
 	private String contactName;
 	private Date dateOfBirth;
@@ -53,6 +61,44 @@ public class ContactInfo implements Comparable<ContactInfo> {
 
 	public int compareTo(ContactInfo another) {
 		return numOfDaysToNextBday - another.numOfDaysToNextBday;
+	}
+
+	public static byte[] toBytes(ContactInfo info) throws IOException
+	{
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+		ObjectOutputStream objectOut = new ObjectOutputStream(out);
+
+		objectOut.writeObject(info);
+
+		objectOut.close();
+
+		return out.toByteArray();
+	}
+
+	public static ContactInfo fromBytes(byte[] bytes) throws Exception
+	{
+		ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+
+		ObjectInputStream oIn = new ObjectInputStream(in);
+
+		ContactInfo info = (ContactInfo) oIn.readObject();
+
+		return info;
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		ContactInfo info = new ContactInfo();
+		info.setContactId("id");
+		info.setContactName("name123");
+
+		byte[] bytes = toBytes(info);
+
+		info = fromBytes(bytes);
+
+		System.out.println(info.getContactId());
+		System.out.println(info.getContactName());
 	}
 
 }
