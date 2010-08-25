e7edd9220b16765a5df7ccad5fa4d96677c27686
rajesh.born2win <rajesh.born2win@9cb73f5c-d2b0-195c-2520-737827032977>
2010-08-25 09:12:02 +0000

    1) fixed issue with leap year. 2) did some code refactoring.

--- trunk/src/com/bdayapp/BdayNotifier.java
+++ trunk/src/com/bdayapp/BdayNotifier.java
@@ -1,32 +1,19 @@
 package com.bdayapp;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
 
 import android.app.Activity;
-import android.content.ContentUris;
-import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.provider.ContactsContract.Data;
-import android.provider.ContactsContract.CommonDataKinds.Event;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.bdayapp.contacts.ContactInfo;
+import com.bdayapp.contacts.ContactListUtil;
 
 public class BdayNotifier extends Activity
 {
 	ArrayAdapter<String> mdArrayAdapter = null;
-	
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,113 +21,12 @@ public class BdayNotifier extends Activity
         setContentView(R.layout.main);
         ListView mContactList;
         mContactList = (ListView)findViewById(R.id.contact_list);
-        Cursor contactCursor = getContactList();
-        int rowNum = contactCursor.getCount();
-        contactCursor.moveToFirst();
-        mdArrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item);
-        ArrayList<contactInfo> contact_list = new ArrayList<contactInfo>();
-        TimeZone tz = TimeZone.getDefault();
-        Calendar presentCal = new GregorianCalendar(tz);
-        for (int i = 0; i < rowNum; i++)
-        {	
-        	contactCursor.moveToPosition(i);
-        	//Log.w("BdayNotifier", "Name = " + contactCursor.getString(1));
-        	String Dob = GetDobOfContact(this, contactCursor.getString(0));
-        	//Log.w("BDayNOtifier", "DOB = " + Dob);
-        	if (!Dob.equalsIgnoreCase(""))
-        	{	
-        		Calendar dobCal = new GregorianCalendar(tz);
-        		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        		Date date = new Date();
-				try {
-					date = (Date)formatter.parse(Dob);
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				Log.w("BdayNotifier", "Date = " + date.toGMTString());
-        		contactInfo cinfo = new contactInfo();
-        		cinfo.contactName = contactCursor.getString(1);
-        		cinfo.dateOfBirth = date;
-        		cinfo.contactID = contactCursor.getString(0);
-        		dobCal.setTime(date);
-        		int pres = 0, dob = 0, diff;
-        		pres = presentCal.get(Calendar.DAY_OF_YEAR);
-        		dob = dobCal.get(Calendar.DAY_OF_YEAR);
-        		diff = pres - dob;
-        		if (diff < 0)
-        		{
-        			cinfo.numOfDaysToNextBday = Math.abs(diff);
-        		}
-        		else
-        		{
-        			cinfo.numOfDaysToNextBday = 365 - diff;
-        		}	
-        		contact_list.add(cinfo);
-        		cinfo.contactPhotoUri = getContactPhoto(this, cinfo.contactID);
-        	}
-        }
-        Collections.sort(contact_list, new contactInfoComparator());
-        Log.w("BdayNotifier", "Num of items in contact_list = " + contact_list.size());
-        
-        bdayList m_Adapter = new bdayList(this, contact_list);
-        mContactList.setAdapter(m_Adapter);
-    }
-    
-    private Cursor getContactList()
-    {
-    	Uri uri = ContactsContract.Contacts.CONTENT_URI;
-    	String[] BdayList = new String[] {
-    			ContactsContract.Contacts._ID,
-    			ContactsContract.Contacts.DISPLAY_NAME,
-        };
-        String selection = ContactsContract.Contacts.IN_VISIBLE_GROUP + " = '" +
-        "1" + "'";
-    	String sortOrder = ContactsContract.Contacts.DISPLAY_NAME;
-    	String[] selectionArgs = null;
-    	return managedQuery(uri, BdayList, selection, selectionArgs, sortOrder);
-    }
-    
-    public String GetDobOfContact(Context ctx, String ContactID)
-    {
-    	String Dob = "";
-        Cursor c = ctx.getContentResolver().query(
-     	       Data.CONTENT_URI,
-     	       new String[] { Event.DATA },
-     	       Data.CONTACT_ID + "=" + ContactID + " AND "
-     	         + Data.MIMETYPE + "= '"
-     	         + Event.CONTENT_ITEM_TYPE + "' AND "
-     	         + Event.TYPE + "=" + Event.TYPE_BIRTHDAY,
-     	       null, Data.DISPLAY_NAME);
-        if (c != null)
-        {
-        	c.moveToFirst();
-        	if (c.getCount() != 0)
-        	{
-        		Dob = c.getString(0);
-        	}
-        	c.close();
-        }
-        return Dob;
-    }
-   private Uri getContactPhoto(Context ctx, String contactID)
-   {
-       Uri person = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, Long
-               .parseLong(contactID));
-       Uri photo = Uri.withAppendedPath(person, ContactsContract.Contacts.Photo.CONTENT_DIRECTORY);
+        ArrayList<ContactInfo> contactList = ContactListUtil.getContactList(this);
 
-       Log.w("getContactPhoto", "photo Uri = " + photo.toString());
-       return photo;
-   }
-}
-class contactInfoComparator implements Comparator<Object>
-{
-	public int compare(Object obj1, Object obj2)
-	{
-		contactInfo c1 = (contactInfo) obj1;
-		contactInfo c2 = (contactInfo) obj2;
-		
-		return (c1.numOfDaysToNextBday - c2.numOfDaysToNextBday);
-	}
+        BDayListAdapter m_Adapter = new BDayListAdapter(this, contactList);
+        mContactList.setAdapter(m_Adapter);
+
+    }
+
 
 }
---
+++ trunk/src/com/bdayapp/contacts/ContactListUtil.java
@@ -0,0 +1,94 @@
+package com.bdayapp.contacts;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+
+import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds.Event;
+import android.provider.ContactsContract.Data;
+import android.util.Log;
+
+import com.bdayapp.Utils;
+
+public class ContactListUtil {
+
+    private static Cursor getContactCursor(Activity activity)
+    {
+    	Uri uri = ContactsContract.Contacts.CONTENT_URI;
+    	String[] BdayList = new String[] {
+    			ContactsContract.Contacts._ID,
+    			ContactsContract.Contacts.DISPLAY_NAME,
+        };
+        String selection = ContactsContract.Contacts.IN_VISIBLE_GROUP + " = '" + "1" + "'";
+    	String sortOrder = ContactsContract.Contacts.DISPLAY_NAME;
+    	String[] selectionArgs = null;
+    	return activity.managedQuery(uri, BdayList, selection, selectionArgs, sortOrder);
+    }
+
+    public static ArrayList<ContactInfo> getContactList(Activity activity)
+    {
+    	Cursor contactCursor = getContactCursor(activity);
+        int rowNum = contactCursor.getCount();
+        contactCursor.moveToFirst();
+
+        ArrayList<ContactInfo> contact_list = new ArrayList<ContactInfo>();
+        for (int i = 0; i < rowNum; i++)
+        {
+        	contactCursor.moveToPosition(i);
+        	//Log.w("BdayNotifier", "Name = " + contactCursor.getString(1));
+        	Date dob = ContactListUtil.getDobOfContact(activity, contactCursor.getString(0));
+        	//Log.w("BDayNOtifier", "DOB = " + Dob);
+        	if (dob != null)
+        	{
+        		Log.w("BdayNotifier", "Date = " + dob.toGMTString());
+        		ContactInfo cinfo = new ContactInfo();
+        		cinfo.setContactName(contactCursor.getString(1));
+        		cinfo.setDateOfBirth(dob);
+        		cinfo.setContactId(contactCursor.getString(0));
+        		cinfo.setContactPhotoUri(ContactListUtil.getContactPhoto(activity, cinfo.getContactId()));
+        		contact_list.add(cinfo);
+        	}
+        }
+        Collections.sort(contact_list);
+        Log.w("BdayNotifier", "Num of items in contact_list = " + contact_list.size());
+
+        return contact_list;
+    }
+
+	public static Date getDobOfContact(Context ctx, String ContactID) {
+		String dateOfBirth = "";
+		Cursor c = ctx.getContentResolver().query(
+				Data.CONTENT_URI,
+				new String[] { Event.DATA },
+				Data.CONTACT_ID + "=" + ContactID + " AND " + Data.MIMETYPE
+						+ "= '" + Event.CONTENT_ITEM_TYPE + "' AND "
+						+ Event.TYPE + "=" + Event.TYPE_BIRTHDAY, null,
+				Data.DISPLAY_NAME);
+		if (c != null) {
+			c.moveToFirst();
+			if (c.getCount() != 0) {
+				dateOfBirth = c.getString(0);
+			}
+			c.close();
+		}
+		return Utils.parse(dateOfBirth);
+	}
+
+	public static Uri getContactPhoto(Context ctx, String contactID) {
+		Uri person = ContentUris.withAppendedId(
+				ContactsContract.Contacts.CONTENT_URI,
+				Long.parseLong(contactID));
+		Uri photo = Uri.withAppendedPath(person,
+				ContactsContract.Contacts.Photo.CONTENT_DIRECTORY);
+
+		Log.w("getContactPhoto", "photo Uri = " + photo.toString());
+		return photo;
+	}
+
+}
