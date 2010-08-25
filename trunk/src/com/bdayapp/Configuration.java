046202eb4753a5c739f884aeb607434b86f2bee7
rajesh.born2win <rajesh.born2win@9cb73f5c-d2b0-195c-2520-737827032977>
2010-08-25 10:16:10 +0000

    rename class & proper code format

--- trunk/src/com/bdayapp/Configuration.java
+++ trunk/src/com/bdayapp/Configuration.java
@@ -11,31 +11,30 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class Configuration extends Activity implements OnClickListener{
+public class Configuration extends Activity implements OnClickListener {
 
 	private NotificationManager mManager;
 
 	public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.configuration);
+		setContentView(R.layout.configuration);
 
-        Button button = (Button) findViewById(R.id.contact_list);
+		Button button = (Button) findViewById(R.id.contact_list);
 
-        button.setOnClickListener(this);
+		button.setOnClickListener(this);
 
-        mManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		mManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 	}
 
 	public void onClick(View v) {
-
-
 
 		Intent i = new Intent(this, BdayNotifier.class);
 
 		Notification note = new Notification(R.drawable.icon, "hehe", System.currentTimeMillis());
 
-		note.setLatestEventInfo(this, "title", "text", PendingIntent.getActivity(this.getBaseContext(), 0, i, PendingIntent.FLAG_CANCEL_CURRENT));
+		note.setLatestEventInfo(this, "title", "text",
+				PendingIntent.getActivity(this.getBaseContext(), 0, i, PendingIntent.FLAG_CANCEL_CURRENT));
 
 		mManager.notify(0, note);
 
