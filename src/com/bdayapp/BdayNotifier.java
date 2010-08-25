0bca667ad028065593f5799f0c937d385e610d0b
psrinivas <psrinivas@gmail.com>
2010-08-25 13:12:37 +0000

    Add new activity for contact details page.
    Note: does not work yet.

--- src/com/bdayapp/BdayNotifier.java
+++ src/com/bdayapp/BdayNotifier.java
@@ -3,14 +3,20 @@ package com.bdayapp;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 
 import com.bdayapp.contacts.ContactInfo;
 import com.bdayapp.contacts.ContactListUtil;
 
 public class BdayNotifier extends Activity {
 
+	public static ArrayList<ContactInfo> contactList;
+	public static int clickPosition = 0;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -18,10 +24,21 @@ public class BdayNotifier extends Activity {
 		setContentView(R.layout.main);
 		ListView mContactList;
 		mContactList = (ListView) findViewById(R.id.contact_list);
-		ArrayList<ContactInfo> contactList = ContactListUtil.getContactList(this);
+		contactList = ContactListUtil.getContactList(this);
 
 		BdayListAdapter m_Adapter = new BdayListAdapter(this, contactList);
 		mContactList.setAdapter(m_Adapter);
+		
+		mContactList.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+			        int position, long id) {
+			      // When clicked, show a toast with the TextView text
+				clickPosition = position;
+				Intent i = new Intent(BdayNotifier.this, ContactPage.class);
+				startActivity(i);
+			    }
+
+		});
 
 	}
 
