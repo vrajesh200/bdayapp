ae4b788891c7c297ea674f78856a2904bc77cdb4
psrinivas <psrinivas@9cb73f5c-d2b0-195c-2520-737827032977>
2010-08-26 15:55:56 +0000

    Added Loading screen. Moved all the processing into Async Thread.

--- trunk/src/com/bdayapp/BdayNotifier.java
+++ trunk/src/com/bdayapp/BdayNotifier.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -27,38 +28,22 @@ public class BdayNotifier extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
 		boolean disableAlarm = true;
-		ListView mContactList;
-		mContactList = (ListView) findViewById(R.id.contact_list);
-		contactList = ContactListUtil.getContactList(this);
+		new LoadTask().execute(this);
 
-		BdayListAdapter m_Adapter = new BdayListAdapter(this, contactList);
-		mContactList.setAdapter(m_Adapter);
 		if (disableAlarm != true)
 		{
 			// get a Calendar object with current time
 			Calendar cal = Calendar.getInstance();
 			// add 5 minutes to the calendar object
 			cal.add(Calendar.MINUTE, 5);
-			Intent intent = new Intent(this, AlarmReceiver.class);
-			intent.putExtra("alarm_message", "O'Doyle Rules!");
-			// In reality, you would want to have a static variable for the request code instead of 192837
+			Intent intent = new Intent(this, AlarmReceiver.class);			// In reality, you would want to have a static variable for the request code instead of 192837
 			PendingIntent sender = PendingIntent.getBroadcast(this, 192837, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 			// Get the AlarmManager service
 			AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
 			am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), sender);
 		}
-		mContactList.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View view,
-			        int position, long id) {
-			      // When clicked, show a toast with the TextView text
-				clickPosition = position;
-				Intent i = new Intent(BdayNotifier.this, ContactPage.class);
-				startActivity(i);
-			    }
-			});
-		}
+	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -76,4 +61,59 @@ public class BdayNotifier extends Activity {
 	   }
 	   return true;
 	}
+	
+	
+	private class LoadTask extends AsyncTask<Activity, Void, ArrayList<ContactInfo>>{
+		
+		@Override
+		protected ArrayList<ContactInfo> doInBackground(Activity... params) {
+			// perform long running operation operation
+			ArrayList<ContactInfo> mcontactList = ContactListUtil.getContactList(params[0]);
+			
+			return mcontactList;
+		}
+
+		/* (non-Javadoc)
+		 * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
+		 */
+		@Override
+		protected void onPostExecute(ArrayList<ContactInfo> result) {
+			// execution of result of Long time consuming operation
+			setContentView(R.layout.main);
+			ListView mContactList;
+			mContactList = (ListView) findViewById(R.id.contact_list);
+			BdayListAdapter m_Adapter = new BdayListAdapter(BdayNotifier.this, result);
+			mContactList.setAdapter(m_Adapter);
+			BdayNotifier.contactList = result;
+			mContactList.setOnItemClickListener(new OnItemClickListener() {
+				public void onItemClick(AdapterView<?> parent, View view,
+				        int position, long id) {
+				      // When clicked, show a toast with the TextView text
+					BdayNotifier.clickPosition = position;
+					Intent i = new Intent(BdayNotifier.this, ContactPage.class);
+					startActivity(i);
+				    }
+				});
+			
+
+		}
+
+		/* (non-Javadoc)
+		 * @see android.os.AsyncTask#onPreExecute()
+		 */
+		@Override
+		protected void onPreExecute() {
+		// Things to be done before execution of long running operation. For example showing ProgessDialog
+			setContentView(R.layout.splashscreen);
+		}
+
+		/* (non-Javadoc)
+		 * @see android.os.AsyncTask#onProgressUpdate(Progress[])
+		 */
+		@Override
+		protected void onProgressUpdate(Void... values) {
+	      // Things to be done while execution of long running operation is in progress. For example updating ProgessDialog
+		 }
+	}
+	
 }
