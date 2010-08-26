efc96e07c2cf2729969c706fa2d14f45bdef1fdc
psrinivas <psrinivas@9cb73f5c-d2b0-195c-2520-737827032977>
2010-08-26 14:32:30 +0000

    Adding hooks for setting alarms to schedule application wakeup.

---
+++ trunk/src/com/bdayapp/AlarmReceiver.java
@@ -0,0 +1,18 @@
+package com.bdayapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		Intent alarmIntent = new Intent(context, BdayNotifier.class);
+		alarmIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		alarmIntent.putExtra("IsAlarmSource", true);
+		context.startActivity(alarmIntent);		
+	}
+
+}
