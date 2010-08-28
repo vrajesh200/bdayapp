e1d761dbd1a032c8bfc1d8deb970a60aeda89dd8
psrinivas <psrinivas@gmail.com>
2010-08-28 21:00:32 +0000

    Add license

--- src/com/bdayapp/AlarmService.java
+++ src/com/bdayapp/AlarmService.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright (C) 2010 Srinivas Paladugu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//=====================================================================================================================
+// $HeadURL: https://bdayapp.googlecode.com/svn/trunk/src/com/bdayapp/AlarmReceiver.java $
+// Checked in by: $Author: psrinivas $
+// $Date: 2010-08-29 02:26:41 +0530 (Sun, 29 Aug 2010) $
+// $Revision: 37 $
+//=====================================================================================================================
+
 package com.bdayapp;
 
 import android.app.IntentService;
